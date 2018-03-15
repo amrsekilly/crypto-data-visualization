9a63bb4618dcc345e3f60f8085172c6fde85ddb6
Amr Elsekilly <amr.e.dev@gmail.com>
2018-03-15 12:43:56 +0200

    added semantic UI to the project

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
- 
+
+  //  Semantic UI settings 
+  $('#slider').cycle({
+    fx: 'fade',
+    pause: 1,
+    next: '#next2',
+    prev: '#prev2',
+    delay: 2000
+  });
+  $(".ui.dropdown").dropdown();
+  $('.ui.sidebar').sidebar();
+
+  
+
 });
