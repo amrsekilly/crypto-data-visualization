b003de3bfae02243110bee8693957fe761ebe1d2
Amr Elsekilly <amr.e.dev@gmail.com>
2018-03-29 16:04:42 +0200

    added the exchange centers for bitcoin

--- js/map.js
+++ js/map.js
@@ -2,309 +2,406 @@
  // import the cryptocurrency icons
 const bitcoin = "/market-data-project/images/icons/btc.png";
 const ethereum = "/market-data-project/images/icons/eth.png";
+const bitcoinCash = "/market-data-project/images/icons/bcc.png";
 const litecoin = "/market-data-project/images/icons/ltc.png";
-const bch = "/market-data-project/images/icons/bch.png";
-const btcz = "/market-data-project/images/icons/btcz.png";
+const ripple = "/market-data-project/images/icons/rep.png";
+const dash = "/market-data-project/images/icons/dash.png";
+
 
 // make sure that the map is loaded to the page
 if ($("#map").length != 0) {
 
+  // the exchange centers 
+  var Bitbank = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin, litecoin, ripple],
+      "country": "Japan",
+      "iconSize": 10
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        139.707731,
+        35.659129
+      ]
+    }
+  };
+
+  var Bitmex = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin],
+      "country": "Seychelles",
+      "iconSize": 30
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        55.491977,
+        4.679574
+      ]
+    }
+  };
+
+  var Bitso = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin, ethereum, bitcoinCash, litecoin, ripple],
+      "country": "Mexico",
+      "iconSize": 10
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        -99.133208,
+        19.432608
+      ]
+    }
+  };
+
+  var Bitstamp = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin, ethereum, bitcoinCash, litecoin, ripple],
+      "country": "Luxembourg",
+      "iconSize": 30
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        6.129583,
+        49.815273
+      ]
+    }
+  };
+
+
+  var BTCBox = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin],
+      "country": "Japan",
+      "iconSize": 20
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        138.252924,
+        36.204824
+      ]
+    }
+  };
+
+  var BTCC = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin, ethereum],
+      "country": "Hong Kong",
+      "iconSize": 20
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        114.109497,
+        22.396428
+      ]
+    }
+  };
+
+  var CEXIO = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin, ethereum, bitcoinCash, ripple, dash],
+      "country": "UK",
+      "iconSize": 10
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        -0.098821,
+        51.523144
+      ]
+    }
+  };
+
+  var Coincheck = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin, ethereum, litecoin, bitcoinCash, ripple, dash],
+      "country": "Japan",
+      "iconSize": 10
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        139.691706,
+        35.689487
+      ]
+    }
+  };
+
+  var Coinfloor = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin, bitcoinCash],
+      "country": "UK",
+      "iconSize": 10
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        -0.112312,
+        51.516728
+      ]
+    }
+  };
+
+  var GoPax = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin, ethereum, litecoin, bitcoinCash],
+      "country": "South Korea",
+      "iconSize": 20
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [127.059108,
+        37.511683
+      ]
+    }
+  };
+
+  var Huobi = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin, ethereum, litecoin, bitcoinCash, ripple, dash],
+      "country": "China",
+      "iconSize": 30
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [116.407396,
+        39.9042
+      ]
+    }
+  };
+
+  var ItBit = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin],
+      "country": "USA",
+      "iconSize": 10
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-73.975427,
+       40.753289
+      ]
+    }
+  };
+
+  var Kraken = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin, ethereum, litecoin, bitcoinCash, ripple, dash],
+      "country": "USA",
+      "iconSize": 30
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-122.400874,
+        37.790047
+      ]
+    }
+  };
+
+  var OKCoin = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin, ethereum, litecoin],
+      "country": "Hong Kong",
+      "iconSize": 10
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [114.182376,
+      22.281485
+      ]
+    }
+  };
+
+  var OKEx = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin, ethereum, litecoin, bitcoinCash, dash],
+      "country": "Belize",
+      "iconSize": 30
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-88.181928,
+        17.493383
+      ]
+    }
+  };
+
+  var SurBTC = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin, ethereum, bitcoinCash],
+      "country": "Chile",
+      "iconSize": 10
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-70.59851,
+        -33.406555
+      ]
+    }
+  };
+
+  var SurBTC2 = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin, ethereum, bitcoinCash],
+      "country": "Colombia",
+      "iconSize": 10
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-74.297333, 4.570868]
+    }
+  };
+
+  var SurBTC3 = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin, ethereum, bitcoinCash],
+      "country": "Peru",
+      "iconSize": 10
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-75.015152, -9.189967]
+    }
+  };
+
+  var TheRock = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin, ethereum, litecoin, bitcoinCash, ripple, dash],
+      "country": "Malta",
+      "iconSize": 10
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [14.514553, 35.898908]
+    }
+  };
+
+  var Unocoin = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin],
+      "country": "India",
+      "iconSize": 10
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [77.559957, 12.985186]
+    }
+  };
+
+  var Vaultoro = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin],
+      "country": "UK",
+      "iconSize": 10
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-3.157272, 51.479222]
+    }
+  };
+
+
+  var Zaif = {
+    "type": "Feature",
+    "properties": {
+      "logo": "/market-data-project/images/partners/bitbank.png",
+      "currencies": [bitcoin, ethereum, bitcoinCash],
+      "country": "Japan",
+      "iconSize": 20
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [135.497341, 34.682592]
+    }
+  };
+
+
   // the places' geojson data for each cryptocurrency
   var bitcoinData = {
     "type": "FeatureCollection",
-    "features": [{
-        "type": "Feature",
-        "properties": {
-          "logo": "https://i.imgur.com/2IVXVHL.png",
-          "currencies": [bitcoin, litecoin, bch, btcz],
-          "country": "Hong Kong",
-          "iconSize": 20
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            20.201469,
-            9.712924
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "logo": "https://i.imgur.com/2IVXVHL.png",
-          "currencies": [bitcoin, ethereum, bch, btcz],
-          "country": "Korea",
-          "iconSize": 10
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            40.201469,
-            9.712924
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "logo": "https://i.imgur.com/2IVXVHL.png",
-          "country": "Thailand",
-          "currencies": [bitcoin, litecoin, ethereum, bch, btcz],
-          "iconSize": 30
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [-63.29223632812499, -18.28151823530889]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "logo": "https://i.imgur.com/2IVXVHL.png",
-          "currencies": [bitcoin, ethereum, bch, btcz],
-          "country": "USA",
-          "iconSize": 10
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [-53.29223632812499, -28.28151823530889]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "logo": "https://i.imgur.com/2IVXVHL.png",
-          "currencies": [bitcoin, litecoin, ethereum, bch],
-          "country": "Russia",
-          "iconSize": 20
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            20.201469,
-            19.712924
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "logo": "https://i.imgur.com/2IVXVHL.png",
-          "currencies": [bitcoin, litecoin, ethereum, btcz],
-          "country": "Chiang Mai",
-          "iconSize": 20
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            49.902097,
-            64.205112
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "logo": "https://i.imgur.com/2IVXVHL.png",
-          "currencies": [bitcoin, litecoin, bch, btcz],
-          "country": "Kansas",
-          "iconSize": 20
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [-96.324462890625,
-            40.024695711685304
-          ]
-        }
-      },
+    "features": [
+      Bitbank,
+      Bitmex,
+      Bitso,
+      Bitstamp,
+      BTCBox,
+      BTCC,
+      CEXIO,
+      Coincheck,
+      Coinfloor,
+      GoPax,
+      Huobi,
+      ItBit,
+      Kraken,
+      OKCoin,
+      OKEx,
+      SurBTC,
+      SurBTC2,
+      SurBTC3,
+      TheRock,
+      Unocoin,
+      Vaultoro,
+      Zaif
     ]
   };
 
   var ethereumData = {
     "type": "FeatureCollection",
-    "features": [{
-      "type": "Feature",
-      "properties": {
-        "logo": "https://i.imgur.com/2IVXVHL.png",
-        "currencies": [bitcoin, litecoin, bch, btcz],
-        "country": "Hong Kong",
-        "iconSize": 20
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          11.201469,
-          9.712924
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "logo": "https://i.imgur.com/2IVXVHL.png",
-        "currencies": [bitcoin, ethereum, bch, btcz],
-        "country": "Korea",
-        "iconSize": 10
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          40.201469,
-          9.712924
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "logo": "https://i.imgur.com/2IVXVHL.png",
-        "country": "Thailand",
-        "currencies": [bitcoin, litecoin, ethereum, bch, btcz],
-        "iconSize": 30
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-63.29223632812499, -18.28151823530889]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "logo": "https://i.imgur.com/2IVXVHL.png",
-        "currencies": [bitcoin, ethereum, bch, btcz],
-        "country": "USA",
-        "iconSize": 10
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-78.29223632812499, -14.28151823530889]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "logo": "https://i.imgur.com/2IVXVHL.png",
-        "currencies": [bitcoin, litecoin, ethereum, btcz],
-        "country": "Chiang Mai",
-        "iconSize": 20
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          55.902097,
-          32.205112
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "logo": "https://i.imgur.com/2IVXVHL.png",
-        "currencies": [bitcoin, litecoin, bch, btcz],
-        "country": "Kansas",
-        "iconSize": 20
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-54.324462890625,
-          40.024695711685304
-        ]
-      }
-    },
+    "features": [
     ]
   };
 
   var usdData = {
     "type": "FeatureCollection",
-    "features": [{
-      "type": "Feature",
-      "properties": {
-        "logo": "https://i.imgur.com/2IVXVHL.png",
-        "currencies": [bitcoin, litecoin, bch, btcz],
-        "country": "Hong Kong",
-        "iconSize": 20
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          21.201469,
-          -9.712924
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "logo": "https://i.imgur.com/2IVXVHL.png",
-        "currencies": [bitcoin, ethereum, bch, btcz],
-        "country": "Korea",
-        "iconSize": 10
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -40.201469,
-          -9.712924
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "logo": "https://i.imgur.com/2IVXVHL.png",
-        "country": "Thailand",
-        "currencies": [bitcoin, litecoin, ethereum, bch, btcz],
-        "iconSize": 30
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-43.29223632812499, -38.28151823530889]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "logo": "https://i.imgur.com/2IVXVHL.png",
-        "currencies": [bitcoin, ethereum, bch, btcz],
-        "country": "USA",
-        "iconSize": 10
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-68.29223632812499, -34.28151823530889]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "logo": "https://i.imgur.com/2IVXVHL.png",
-        "currencies": [bitcoin, litecoin, ethereum, btcz],
-        "country": "Chiang Mai",
-        "iconSize": 20
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          85.902097,
-          12.205112
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "logo": "https://i.imgur.com/2IVXVHL.png",
-        "currencies": [bitcoin, litecoin, bch, btcz],
-        "country": "Kansas",
-        "iconSize": 20
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-44.324462890625,
-          10.024695711685304
-        ]
-      }
-    },
+    "features": [
     ]
   };
 
