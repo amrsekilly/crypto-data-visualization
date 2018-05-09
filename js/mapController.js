e739ff38b77232c121efd9f82ee08100d40bb583
Amr Elsekilly <amr.e.dev@gmail.com>
2018-05-09 21:28:31 +0200

    modified the circle styles for the map

--- js/mapController.js
+++ js/mapController.js
@@ -546,7 +546,7 @@ if ($("#map").length != 0) {
       text: "> $9B",
       color: "#77E2FC"
     }].forEach(function (radius) {
-    legend.insertAdjacentHTML('beforeend', '<span class="circle" style="width:' + radius.size + 'px; background-color: '+ radius.color +'; top: 2px; height:' + radius.size + 'px;"></span><span>'+ radius.text +'</span>');
+    legend.insertAdjacentHTML('beforeend', '<span class="circle" style="width:' + radius.size + 'px; background: linear-gradient(to bottom right,' +  radius.color + ', white); top: 2px; height:' + radius.size + 'px;"></span><span>' + radius.text + '</span>');
   });
 
 
@@ -559,6 +559,10 @@ if ($("#map").length != 0) {
       "type": "circle",
       "paint": {
         "circle-radius": 8,
+        "circle-stroke-width": 2,
+        "circle-stroke-color": "#FFF",
+        "circle-opacity": 0.9,
+        "circle-blur": 0.4,
         "circle-color": {
           property: "iconSize",
           stops: [
